9ddd4b38b101daa68eac27c4a0f49c61dc79e549
eneumont <129240077+eneumont@users.noreply.github.com>
2024-01-18 13:22:09 -0700

    basics
    basic movement works now need to do battle scripts

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveCamera : MonoBehaviour {
-    public Transform cameraPosition;
+    [SerializeField] Transform target = null;
+    [SerializeField][Range(20, 90)] float defaultPitch = 40;
+    [SerializeField][Range(2, 8)] float distance = 6;
+    [SerializeField][Range(0.1f, 2.0f)] float sensitivity = 1;
 
-    // Start is called before the first frame update
-    void Start() {
-        
+    float yaw = 0;
+    float pitch = 0;
+
+    void Start()
+    {
+        pitch = defaultPitch;
     }
+    void Update()
+    {
+        yaw += Input.GetAxis("Mouse X") * sensitivity;
+        pitch += Input.GetAxis("Mouse Y") * sensitivity;
 
-    // Update is called once per frame
-    void Update() {
-        transform.position = cameraPosition.position;
+        Quaternion qyaw = Quaternion.AngleAxis(yaw, Vector3.up);
+        Quaternion qpitch = Quaternion.AngleAxis(pitch, Vector3.right);
+        Quaternion rotation = qyaw * qpitch;
+
+        transform.position = target.position + (rotation * Vector3.back * 5);
+        transform.rotation = rotation;
     }
 }
--- Assets/Scripts/playerMove.cs
+++ Assets/Scripts/playerMove.cs
@@ -3,11 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class playerMove : MonoBehaviour {
-    void Start() {
-        
+    [Header("Movement")]
+    [SerializeField][Range(1, 10)] float maxForce;
+    [SerializeField][Range(1, 10)] float jumpForce;
+    [SerializeField] Transform view;
+    [Header("Collision")]
+    [SerializeField][Range(0, 5)] float raylength = 1;
+    [SerializeField] LayerMask groundLayerMask;
+
+    Rigidbody rb;
+    Vector3 force = Vector3.zero;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
     }
 
-    void Update() {
-        
+    void Update()
+    {
+        Vector3 direction = Vector3.zero;
+        direction.x = Input.GetAxis("Horizontal");
+        direction.z = Input.GetAxis("Vertical");
+
+        Quaternion yrotation = Quaternion.AngleAxis(view.rotation.eulerAngles.y, Vector3.up);
+        force = yrotation * direction * maxForce;
+
+        Debug.DrawRay(transform.position, Vector3.down * raylength, Color.red);
+        if (Input.GetButtonDown("Jump") && CheckGround())
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(force, ForceMode.Force);
+    }
+
+    bool CheckGround()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, raylength, groundLayerMask);
     }
 }
